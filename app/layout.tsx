33450a447743e0f2817b84f7e32b709094d0e6a7
Koder2704 <danielkembeu82@gmail.com>
2024-03-17 12:58:37 +0100

    FIRST COMMIT FOR DEPLOYMENT

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+// Adding global css to all the routes.
+import "@/app/ui/global.css";
+
+// Using the google inter font
+import { inter } from "./ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
